cb4e23fd323a05e5f5be1fa9021dd3ceca4c081f
gencurio <gonzalo.ar18@hotmail.com>
2018-04-21 00:08:07 -0700

    initial commit

---
+++ app/src/main/java/com/example/garana/fireflight/mapLayout.java
@@ -0,0 +1,13 @@
+package com.example.garana.fireflight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mapLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_layout);
+    }
+}
